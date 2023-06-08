0b48c33120e183420e7de900c89b8fc5873e370c
kennymaur <kehinde.afolabi@azubiafrica.org>
2023-06-08 20:47:31 +0100

    streamlit app modified

--- src/app.py
+++ src/app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
-st.write("""#My Interface
-         This is my interface using Streamlit.
-         """)
+st.write(
+"""#My Interface
+ This is my interface using Streamlit.
+"""
+)
+
+sepal_height = st.number_input("Enter sepal height")
+sepal_width = st.number_input("Enter sepal width")
+
+petal_height = st.number_input("Enter petal height")
+petal_width = st.number_input("Enter petal width")
+
+
+if st.button("Predict"):
+    st.text(f"The iris has been classified as : '{''}' .")
